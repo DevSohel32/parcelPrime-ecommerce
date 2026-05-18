26a0cc2ec404db0c056937d73cbafff8dc194b3d
DevSohel32 <mdsujauddoulaoshel@gmail.com>
2026-05-18 06:02:47 +0600

    feat: add homepage, controllers, and authAdmin middleware
    - Create homepage view and design.
    - Implement UserController and AdminController for handling routing logic.
    - Create authAdmin middleware for administrative route protection.

---
+++ resources/views/admin/index.blade.php
@@ -0,0 +1,6 @@
+@extends('layouts.app')
+@section('content')
+<div>
+    <h1>admin Dashboard</h1>
+</div>
+@endsection
---
+++ resources/views/user/index.blade.php
@@ -0,0 +1,6 @@
+@extends('layouts.app')
+@section('content')
+<div>
+    <h1>User Dashboard</h1>
+</div>
+@endsection
